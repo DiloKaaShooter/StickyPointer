27c20fa6696bc94d2aa2f42d6e2baf8ec2a59ffe
Vaibhav Singh <119793533+DiloKaaShooter@users.noreply.github.com>
2024-05-14 02:43:13 +0530

    added the buying component in app remotely

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
-
 function App() {
+  const text = "Red Snickers";
   return (
-    <>
-
-    </>
-  )
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className=" text-[3rem] text-[#fefefe14] font-bold">{text}</h1>
+      <h1 className=" text-[3rem] text-[#fffbfb48] font-bold relative bottom-7">{text}</h1>
+      <h1 className=" text-[3rem] text-[#ffffffec] font-bold relative bottom-14">{text}</h1>
+      <form className="flex p-8 rounded-xl flex-col min-w-[400px] w-[50%] h-[26rem] justify-center items-center bg-[#f5f0f0de]">
+        <div className="product rounded h-[35rem] w-[90%] bg-cover bg-center" style={{backgroundImage: "url('/shoes.jpg')"}}></div>
+        <input type="submit" value="Submit" className="mt-5 bg-gray-900 p-5 py-8 rounded-full text-[#ffffffe6]" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
